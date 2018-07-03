200e4a39713a9b5d5c7a628a767054b02290575e
James <James.Miller@momentumww.com>
2018-07-03 10:51:22 +0100

    jenkins integration

--- web/react/src/client/pages/home/HomePage.js
+++ web/react/src/client/pages/home/HomePage.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import './HomePage.scss';
 import './react.png';
-import { BrowserView, TabletView, MobileView, browserName, CustomView } from "react-device-detect";
+import { BrowserView, TabletView, MobileOnlyView, browserName, CustomView } from "react-device-detect";
 
 class Home extends Component {
   render() {
     return (
-
       <BrowserView>  
         <div className = "class"> 
           <h3> Home page </h3>
           <p> Built in ssr React and Redux </p>
           <img src="/assets/images/react.png" />
         </div>
-      </BrowserView>
-      // ,
-      // <CustomView condition={browserName === "Chrome"}>
-      //   <div>"lol?"</div>
-      // </CustomView>
+      </BrowserView>,
+      // get browsername from action http request
+      <CustomView condition={browserName === "Chrome"}>
+        <div>"lol?"</div>
+      </CustomView>
+      
 	  )
   }
 };
