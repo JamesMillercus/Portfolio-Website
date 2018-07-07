a21d1b4b7819d7bc6fe8d5ce03a857aba778e2f9
James <James.Miller@momentumww.com>
2018-07-07 13:40:22 +0100

    User Agent now working on different devices

--- web/react/src/client/components/hocs/getDevice.js
+++ web/react/src/client/components/hocs/getDevice.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import GetBrowser from './getBrowser';
-import { BrowserView, TabletView, MobileOnlyView } from "react-device-detect";
+import { UserAgent } from '@quentin-sommer/react-useragent';
+
 
 /*
 	TO DO:
-		1. CREATE (AND TEST) MOBILE AND TABLET VIEWS
+		1. TEST MOBILE AND TABLET VIEWS
 		2. ATTEMPT TO STORE USER AGENT (FROM APP.JS) IN REDUX STORE AND LOAD HOME PAGE ONCE THIS DATA IS LOADED INTO FRONT END
 		3. DO THIS BY STORING THE USER AGENT IN REDUX STATE AND THEN REFERENCE THAT STATE IN APP.JS
 */
@@ -14,13 +15,24 @@ export default (ChildComponent) => {
 	class GetDevice extends Component {
 		constructor(props) {
 	        super(props);
-	        this.deviceComponent = () => {
-	    		return (
-					<BrowserView>
+    	}
+
+        deviceComponent () {
+    		return (
+    			<div>
+		            <UserAgent computer>
 						<GetBrowser />
-					</BrowserView>
-	    		)
-	    	}
+					</UserAgent>
+
+		            <UserAgent tablet>
+						<div> Tablet view </div>
+					</UserAgent>
+
+					<UserAgent mobile>
+						<div> Mobile view </div>
+					</UserAgent>
+				</div>
+    		)
     	}
 
 		render() {
