86cfaecfa411eb317be2b4a692f287745b751e26
James <James.Miller@momentumww.com>
2018-07-23 22:55:11 +0100

    ADT asset created and integrated

--- web/react/src/server/helpers/contentRes.js
+++ web/react/src/server/helpers/contentRes.js
@@ -24,9 +24,7 @@ module.exports = (app, req, res) => {
 
 	// once all our data requests have been completed
 	Promise.all(promises).then(() => {
-		const context = {
-			userAgent: req.headers['user-agent']
-		};
+		const context = {};
 		const content = renderer(req, store, context);
 		// if a url is loaded into the context then redirect to the url in the context
 		if (context.url) return res.redirect(301, context.url);
