6df720b2e36a065273b2a959752aed9cda1be4ff
James <James.Miller@momentumww.com>
2018-07-31 21:20:44 +0100

    LOGO FINISHED WOO!!
    Next steps:
    	1. make top and bottom hero texts closer to the icons
    	2. re-adjust item text so it looks neater for all items
    	3. set heroLogo to expand to james miller on scroll over
    	4. when scrolled out return to logo
    	5. set hero text to normal font when icon scroll occurs
    	6. set james miller to animate when scrolled over
    	7. set james miller to change colour when clicked
    	8. hero text change on item scroll over
    	9. hero text animation on change

--- web/react/src/client/components/locs/GridContainer/Hero/HeroText/assets/config/heroTextConfig.js
+++ web/react/src/client/components/locs/GridContainer/Hero/HeroText/assets/config/heroTextConfig.js
@@ -1,37 +1,38 @@
 export default {
 	none: {
-		header: 'Freelance creative technologist',
-		paragraph: ['J', 'a', 'm', 'e', 's', ' ', 'M', 'i', 'l', 'l', 'e', 'r'],
-		footer: 'Design, development and consulting'
+		header: 'Freelance designer and creative technologist',
+		// paragraph: ['j', 'a', 'm', 'e', 's', ' ', 'm', 'i', 'l', 'l', 'e', 'r'],
+		paragraph: ['j', 'm'],
+		footer: 'I help you bring technology ideas to life!'
 	},
 	topLeftIcon: {
 		header: 'Drop me an email and say',
 		paragraph: ['h', 'i', '@', 'j', 'a', 'm', 'e', 's', 'm', 'i', 'l', 'l', 'e', 'r', '.', 'd', 'e', 's', 'i', 'g', 'n'],
-		footer: 'to tell me about your ideas!'
+		footer: 'to tell me about your ideas or brief!'
 	},
 	topCenterIcon: {
-		header: 'Lets visualise how your idea will work with',
-		paragraph: ['I', 'n', 't', 'e', 'r', 'a', 'c', 't', 'i', 'o', 'n', ' ', 'D', 'e', 's', 'i', 'g', 'n'],
+		header: 'Lets visualise how the ideas could work with',
+		paragraph: ['i', 'n', 't', 'e', 'r', 'a', 'c', 't', 'i', 'o', 'n', ' ', 'd', 'e', 's', 'i', 'g', 'n'],
 		footer: 'using Adobe Photoshop, Sketch and Framer.'
 	},
 	topRightIcon: {
 		header: 'Turn the idea into an IoT product or installation with',
-		paragraph: ['E', 'l', 'e', 'c', 't', 'r', 'o', 'n', 'i', 'c', 's'],
+		paragraph: ['e', 'l', 'e', 'c', 't', 'r', 'o', 'n', 'i', 'c', 's'],
 		footer: 'using Arduino, Raspberry Pi, sensors and actuators.'
 	},
 	bottomLeftIcon: {
-		header: 'Launch the idea as an AR, VR, or Blockchain app with',
-		paragraph: ['D', 'e', 'v', 'e', 'l', 'o', 'p', 'm', 'e', 'n', 't'],
-		footer: 'using React(JS/Native), K8, Solidity and more.. '
+		header: 'Launch the idea as an AR, VR, or Blockchain app by',
+		paragraph: ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'i', 'n', 'g'],
+		footer: 'with React(JS/Native), AR Kit, Solidity and more.. '
 	},
 	bottomCenterIcon: {
-		header: 'If we haven\'t already, lets connect and ',
-		paragraph: ['C', 'o', 'l', 'l', 'a', 'b', 'o', 'r', 'a', 't', 'e'],
+		header: 'Lets connect and ',
+		paragraph: ['c', 'o', 'l', 'l', 'a', 'b', 'o', 'r', 'a', 't', 'e'],
 		footer: 'together to make amazing stuff!'
 	},
 	bottomRightIcon: {
 		header: 'Stay in touch to see the latest',
-		paragraph: ['U', 'p', 'd', 'a', 't', 'e', 's'],
+		paragraph: ['u', 'p', 'd', 'a', 't', 'e', 's'],
 		footer: 'and ideas within creative technology!'
 	}
 };
