4d1437901017c19e936e41ab1f368756110a7c2e
James <James.Miller@momentumww.com>
2018-10-02 09:45:31 +0100

    remove os

--- web/react/src/client/components/hocs/getOSVersion.js
+++ web/react/src/client/components/hocs/getOSVersion.js
@@ -31,7 +31,7 @@ export default (ChildComponent) => {
 				compatible = 103; // 10.3
 			}
 
-      if (osFloat <= compatible) return false;
+      // if (osFloat <= compatible) return false;
       return true;
     }
 
